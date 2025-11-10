0b4546d611cb234408683440aeb45442303fd17c
Nitesh47kumars <niteshshah028@gmail.com>
2025-11-10 20:17:43 +0530

    add ScrollToTop Component for UX

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -2,16 +2,16 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "./Components/Navbar/Navbar";
 import Footer from "./Components/Footer/Footer";
+import ScrollToTop from "./ScrollToTop"
 
 const Layout = () => {
   return (
     <div className="min-h-screen flex flex-col">
+      <ScrollToTop/>
       <Navbar />
-
       <main className="grow">
         <Outlet />
       </main>
-
       <Footer />
     </div>
   );
